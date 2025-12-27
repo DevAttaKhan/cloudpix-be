8201dd61a38d85447d554d9f9de584d9f48621f7
AttakhanDev <Attakhandev@gmail.com>
2025-12-27 14:52:29 +0500

    update

--- src/common/util/validators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformIsDate = void 0;
-const jet_validators_1 = require("jet-validators");
-const utils_1 = require("jet-validators/utils");
-/**
- * Convert to date object then check is a validate date.
- */
-exports.transformIsDate = (0, utils_1.transform)((arg) => new Date(arg), (arg) => (0, jet_validators_1.isDate)(arg));
